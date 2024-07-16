2e10589cd34b373e4970737f83319e505319699a
Andonys24 <andonihernandez1990@gmail.com>
2024-07-16 03:55:45 -0600

    Creacion de los proyectos desde el dashboard - Creacion del formulario - Creacion de la tabla de Proyectos en la base de datos - Creacion del Modelo de Proyectos - Validaciones para crear un proyecto Nuevo - Validando que la ruta del proyecto y si le pertenece al usuario - Creando una URL para los proyectos
    Creacion de la vista para proyectos
    - Validando el Proyecto
    - Obteniendo los proyectos del usuario
    - Listando todos los proyectos del usuario
    - Aplicando CSS a los proyectos

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace Controllers;
 
+use Model\Proyecto;
 use MVC\Router;
 
 class DashboardController
@@ -10,8 +11,10 @@ class DashboardController
     {
         session_start();
         isAuth();
+        $proyectos = Proyecto::belogsTo('propietarioId', $_SESSION['id']);
         $router->render('dashboard/index', [
-            'titulo' => 'Proyectos'
+            'titulo' => 'Proyectos',
+            'proyectos' => $proyectos
         ]);
     }
 
@@ -19,8 +22,42 @@ class DashboardController
     {
         session_start();
         isAuth();
+        $alertas = [];
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $proyecto = new Proyecto($_POST);
+            // validacion
+            $alertas = $proyecto->validarProyecto();
+
+            if (empty($alertas)) {
+                // Generar URL unico
+                $proyecto->url = md5(uniqid());
+                // Almacenar creador del proyecto
+                $proyecto->propietarioId = $_SESSION['id'];
+                // guardar el proyecto
+                $proyecto->guardar();
+                // Redireccionar
+                header('Location: /proyecto?id=' . $proyecto->url);
+            }
+        }
         $router->render('dashboard/crear-proyecto', [
-            'titulo' => 'Crear Proyecto'
+            'titulo' => 'Crear Proyecto',
+            'alertas' => $alertas
+        ]);
+    }
+
+    public static function proyecto(Router $router)
+    {
+        session_start();
+        isAuth();
+        $token = $_GET['id'];
+        if (!$token) header('Location: /dashboard');
+        $proyecto = Proyecto::where('url', $token);
+        if ($proyecto->propietarioId !== $_SESSION['id']) {
+            header('Location: /dashboard');
+        }
+        // Revisar que la persona que visita el proyecto es quien lo creo
+        $router->render('dashboard/proyecto', [
+            'titulo' => $proyecto->proyecto
         ]);
     }
 
---
+++ models/Proyecto.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Model;
+
+class Proyecto extends ActiveRecord
+{
+    protected static $tabla = 'proyectos';
+    protected static $columnasDB = ['id', 'proyecto', 'url', 'propietarioId'];
+    public $id;
+    public $proyecto;
+    public $url;
+    public $propietarioId;
+
+    public function __construct($args = [])
+    {
+        $this->id = $args['id'] ?? null;
+        $this->proyecto = $args['proyecto'] ?? '';
+        $this->url = $args['url'] ?? '';
+        $this->propietarioId = $args['propietarioId'] ?? null;
+    }
+
+    public function validarProyecto()
+    {
+        switch (true) {
+            case empty($this->proyecto):
+                self::$alertas['error'][] = 'El nombre del proyecto es Obligatorio';
+                break;
+            case strlen($this->proyecto) > 60:
+                self::$alertas['error'][] = 'El nombre del proyecto no puede tener mas de 60 caracteres';
+                break;
+        }
+        return self::$alertas;
+    }
+}
